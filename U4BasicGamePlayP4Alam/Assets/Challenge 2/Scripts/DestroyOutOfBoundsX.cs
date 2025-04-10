b3d91a0e970ec7c135d9f1c1f311976207510c71
TkAli24 <741712@student.dusd.net>
2025-04-10 11:44:14 -0700

    done

---
+++ U4BasicGamePlayP4Alam/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    public GameObject ground;
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == ground)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
